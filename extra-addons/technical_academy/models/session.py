6bc18625fb3eafd8903a6ab0d6ebb4e2480cf296
Abu Abdirohman <abuabdirohman4@gmail.com>
2023-04-09 05:39:05 +0700

    day 3 : add session attendee

--- extra-addons/technical_academy/models/session.py
+++ extra-addons/technical_academy/models/session.py
@@ -11,3 +11,13 @@ class Session(models.Model):
     session_date = fields.Date(string='Tanggal', default=fields.Date.today)
     min_attendee = fields.Integer(string='Minimal', default=0, required=True)
     description = fields.Text(string='Deskripsi')
+    attendee_ids = fields.One2many(comodel_name='tech.session.attendee', inverse_name='session_id', string='Data Peserta')
+
+class SessionAttendee(models.Model):
+    _name = 'tech.session.attendee'
+    _description = 'Peserta di sesi kursus'
+
+    name = fields.Char(string='No Pendaftaran', required=True)
+    student_id = fields.Many2one(comodel_name='res.partner', string='Siswa', domain="[('is_student', '=', True)]", required=True)
+    description = fields.Text(string='Deskripsi')
+    session_id = fields.Many2one(comodel_name='tech.session', string='Sesi Kursus')
