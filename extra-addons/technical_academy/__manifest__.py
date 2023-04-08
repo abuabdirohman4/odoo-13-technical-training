1d1a55cfc44de966693c2743af49eafa87bf3f2d
Abu Abdirohman <abuabdirohman4@gmail.com>
2023-04-08 11:19:19 +0700

    day 2 : buat view category, view menu category & security access model category

--- extra-addons/technical_academy/__manifest__.py
+++ extra-addons/technical_academy/__manifest__.py
@@ -10,8 +10,8 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Abu Abdirohman",
+    'website': "https://www.abuabdirohman.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -24,9 +24,10 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        # 'views/views.xml',
+        # 'views/templates.xml',
+        'views/category.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
