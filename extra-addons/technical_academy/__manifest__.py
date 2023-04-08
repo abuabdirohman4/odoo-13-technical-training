c224959c478dbcd49c1b8f12f21e58c44109294c
Abu Abdirohman <abuabdirohman4@gmail.com>
2023-04-08 13:01:52 +0700

    day 2 : memisahkan menu pada file sendiri

--- extra-addons/technical_academy/__manifest__.py
+++ extra-addons/technical_academy/__manifest__.py
@@ -27,6 +27,7 @@
         'security/ir.model.access.csv',
         # 'views/views.xml',
         # 'views/templates.xml',
+        'views/menu.xml',
         'views/category.xml',
         'views/course.xml',
     ],
