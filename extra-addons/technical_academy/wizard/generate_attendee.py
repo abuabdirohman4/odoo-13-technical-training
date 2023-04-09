3c34a5318ce8efd516be52ab665240f5a99ea18f
Abu Abdirohman <abuabdirohman4@gmail.com>
2023-04-09 16:32:01 +0700

    day 4 : 15. Wizard Popup Get Default Value Active

--- extra-addons/technical_academy/wizard/generate_attendee.py
+++ extra-addons/technical_academy/wizard/generate_attendee.py
@@ -4,5 +4,11 @@ class SessionWIzard(models.TransientModel):
     _name = 'session.wizard'
     _description = 'Popup untuk generate attendee'
 
-    session_id = fields.Many2one(comodel_name='tech.session', string='Sesi Kursus', required=True)
-    student_ids = fields.Many2many(comodel_name='res.partner', string='Siswa')
+    def _default_session(self):
+        return self.env['tech.session'].browse(self._context.get('active_id'))
+    
+    session_id = fields.Many2one(comodel_name='tech.session', string='Sesi Kursus', required=True, default=_default_session)
+    student_ids = fields.Many2many(comodel_name='res.partner', string='Siswa', domain="[('is_student', '=', True)]")
+
+    def gen_attendee(self):
+        pass
